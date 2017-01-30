775e416f2e514459431247109391aab6f5fa5348
Dmitry Moskowski <me+material-ui-error-reporting@corpix.ru>
2017-01-30 13:37:22 +0000

    Do not show action if it is empty

--- src/ErrorReporting/index.js
+++ src/ErrorReporting/index.js
@@ -11,7 +11,11 @@ class ErrorReporting extends Component {
         action: '',
         error: null,
         autoHideDuration: 10000,
-        getMessage: (props) => props.error ? props.action + ': ' + props.error.toString() : '',
+        getMessage: (props) => props.error
+            ? (
+                (props.action ? props.action + ': ' : '')
+                    + String(props.error)
+            ) : '',
         style: {
             backgroundColor: red900,
             color: grey50
